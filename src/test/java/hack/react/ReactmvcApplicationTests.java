4756e02143fd7131c312c37f5f38941310d70a1f
Khalilur Rahman <khal.rahman@gmail.com>
2016-04-19 01:38:48 +0800

    init spring boot

---
+++ src/test/java/hack/react/ReactmvcApplicationTests.java
@@ -0,0 +1,18 @@
+package hack.react;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ReactmvcApplication.class)
+@WebAppConfiguration
+public class ReactmvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
